bdd0a493334e607c1e76d1a93a4743b4e9f0f14b
cnfc <stdashkov@mail.ru>
2018-09-12 19:33:56 +0300

    create and read table chess

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 function show_map(){
-  html="<table border='1'>";
-  for(let y = 7; y > 0; y--) {
+  html="<table border='1' cellpadding='2' cellspacing='0'>";
+  for(var y = 7; y > 0; y--) {
     html += "<tr>"
-    for(let x = 0; x<=7; x++) {
-      html += "<td></td>";
+    html += "<td>" + y + "</td>"
+    for(var x = 0; x<=7; x++) {
+      color = ( x + y) % 2 == 0 ? "white" : "yellow";
+      html += "<td style='height: 50px; width: 50px; " + "background-color: " + color + "'></td>";
     }
     html += "</tr>";
+  }
+
+  html += "<tr>"
+  html += "<td>&nbsp;</td>";
+  for(var x = 0; x<=7; x++) {
+    html += "<td>" + x + "</td>";
+
   }
   document.getElementById('board').innerHTML = html;
 }
