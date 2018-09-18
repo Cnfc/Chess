3d2c50b5fcf7a941bf8d735770849436baf672ed
cnfc <stdashkov@mail.ru>
2018-09-18 18:28:28 +0300

    restart and add header logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Header';
+import './resources/styles.css';
+import { Element } from 'react-scroll';
 
+
+import Header from './components/header_footer/Header';
+// import Featured from './components/featured';
+// import VunueNfo from './components/venueNfo';
+// import Highlight from './components/Highlights';
+// import Pricing from './components/pricing';
+// import Location from './components/location';
+// import Footer from './components/header_footer/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Header/>
-        
       </div>
     );
   }
