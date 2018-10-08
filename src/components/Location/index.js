b47f8112d9781fba7cde60e0752c2e9381a207ac
cnfc <stdashkov@mail.ru>
2018-10-08 14:58:59 +0300

    complete site

--- src/components/Location/index.js
+++ src/components/Location/index.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 const Location = () => {
   return (
     <div className="location">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d128083.26573163123!2d10.645033699622648!3d59.89395286100945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416e61f267f039%3A0x7e92605fd3231e9a!2z0J7RgdC70L4!5e0!3m2!1sru!2sno!4v1538998776224"
-         width="100%"
-         height="500"
-         frameBorder="0"
-         allowFullScreen>
+      <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63499.5759094865!2d-96.82638045433593!3d32.81400551924428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e993d0ea3c1ff%3A0x1859d2cc74f588e2!2sThe+Dallas+World+Aquarium!5e0!3m2!1ses!2sar!4v1530636115082"
+      width="100%"
+      height="500px"
+      frameBorder="0"
+      allowFullScreen
+
        </iframe>
 
        <div className='location_tag'>
